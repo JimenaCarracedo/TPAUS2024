879024c332433b0584c760d4bb086fc525e908c8
Jimena Carracedo <91408045+JimenaCarracedo@users.noreply.github.com>
2024-07-01 02:58:56 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -5,9 +5,9 @@
 #define J (10)
 int jugador1[J]={-1,-1,-1,-1,-1,-1,-1,-1, -1,-1};
 int jugador2[J]={-1,-1,-1,-1,-1,-1,-1,-1, -1,-1};
-char *juego[J]={"1       ", "2       ", "3       ", "4       ", "5       ", "6        ", "Generala", "Escalera", "Pocker", "Full"};
+char *juego[J]={"1       ", "2       ", "3       ", "4       ", "5       ", "6        ", "G       ", "E        ", "P       ", "F       "};
 int lanzada[P];
-int puntaje_del_juego[J]={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+int puntaje_del_juego[J];
 int lanzar_dado(int lanzada[P]){
 
     srand(time(NULL));
@@ -32,12 +32,9 @@ int lanzar_dado(int lanzada[P]){
         do{
         printf("Cual? 1-2-3-4-5 ---> 0  para no ingresar mas dados \n");
             cont1--;
-
             scanf("%d", &d);
             lanzada[d-1]=(rand()%6)+1;
             }while(d!=0);
-
-
         }
         for(int j=0; j<P; j++){
             printf("%d ", lanzada[j]);
@@ -48,16 +45,13 @@ return cont1;
     }
 
 int anotar(int lanzar_dado(int lanzada), int puntaje_del_juego[J]){
-        int cont_juego_anotado;
         int j;
         int i;
         int posicion_juego_anotado;
         char k;
         do{
-        printf("que juego desea anotar? G para Generala, E para Escalera,  F para Full\n");
+        printf("que juego desea anotar? G para Generala, E para Escalera,  F para Full, P para Pocker\n");
         scanf(" %c", &k);
-
-
         switch(k){
 
         case 49:
@@ -67,17 +61,10 @@ int anotar(int lanzar_dado(int lanzada), int puntaje_del_juego[J]){
             posicion_juego_anotado=0;
             puntaje_del_juego[0]=0;
             for(i=0; i<P; i++){
-
                 if(j==lanzada[i]){
-
                 puntaje_del_juego[0]=puntaje_del_juego[0]+lanzada[i];
             }
-            cont_juego_anotado++;
          }
-
-
-
-
         }
         break;
         case 50:
@@ -87,81 +74,60 @@ int anotar(int lanzar_dado(int lanzada), int puntaje_del_juego[J]){
             posicion_juego_anotado=1;
          puntaje_del_juego[1]=0;
             for(i=0; i<P; i++){
-
                 if(j==lanzada[i]){
-
                 puntaje_del_juego[1]=puntaje_del_juego[1]+lanzada[i];
             }
-
          }
-
-            cont_juego_anotado++;
             }
             break;
 
         case 51:
             if(puntaje_del_juego[2]==-1){
-
             j=3;
             posicion_juego_anotado=2;
             puntaje_del_juego[2]=0;
             for(i=0; i<P; i++){
-
                 if(j==lanzada[i]){
-
                 puntaje_del_juego[2]=puntaje_del_juego[2]+lanzada[i];
             }
          }
-            cont_juego_anotado++;
             }
             break;
         case 52:
             if(puntaje_del_juego[3]==-1){
-
             j=4;
             posicion_juego_anotado=3;
             puntaje_del_juego[3]=0;
             for(i=0; i<P; i++){
-
                 if(j==lanzada[i]){
-
                 puntaje_del_juego[3]=puntaje_del_juego[3]+lanzada[i];
             }
          }
-                     cont_juego_anotado++;
             }
             break;
 
         case 53:
             if(puntaje_del_juego[4]==-1){
-
             j=5;
             posicion_juego_anotado=4;
             puntaje_del_juego[4]=0;
             for(i=0; i<P; i++){
-
                 if(j==lanzada[i]){
-
                 puntaje_del_juego[4]=puntaje_del_juego[4]+lanzada[i];
             }
          }
-                     cont_juego_anotado++;
             }
             break;
         case 54:
             if(puntaje_del_juego[5]==-1){
-
             j=6;
             posicion_juego_anotado=5;
             puntaje_del_juego[5]=0;
             for(i=0; i<P; i++){
-
                 if(j==lanzada[i]){
-
                 puntaje_del_juego[5]=puntaje_del_juego[5]+lanzada[i];
             }
          }
-                     cont_juego_anotado++;
             }
             break;
         case 71:
@@ -185,10 +151,7 @@ int anotar(int lanzar_dado(int lanzada), int puntaje_del_juego[J]){
                 }else if(es==1){
                     puntaje_del_juego[6]=0;
                 }
-
-            cont_juego_anotado++;
             }
-
             break;
 
         case 69:
@@ -204,17 +167,12 @@ int anotar(int lanzar_dado(int lanzada), int puntaje_del_juego[J]){
                         aux=lanzada[i];
                         lanzada[i]=lanzada[j];
                         lanzada[j]=aux;
-
                     }
-
                 }
             }
-
             for(i=0; i<P-1; i++){
-
                    if((lanzada[i]+1)!=lanzada[i+1]){
                     es=1;
-
             }
             }
             if(es==0){
@@ -223,7 +181,6 @@ int anotar(int lanzar_dado(int lanzada), int puntaje_del_juego[J]){
                     puntaje_del_juego[7]=puntaje_del_juego[7]+5;
                 }
             }
-                        cont_juego_anotado++;
             }
             break;
         case 80:
@@ -237,22 +194,20 @@ int anotar(int lanzar_dado(int lanzada), int puntaje_del_juego[J]){
                         aux=lanzada[i];
                         lanzada[i]=lanzada[j];
                         lanzada[j]=aux;
-
                     }
                 }
                 }
                 int cont=0;
                 for(i=0; i<P; i++){
-                for(j=i+1; j<P-1; j++){
+                for(j=i+1; j<P; j++){
                         if(lanzada[i]==lanzada[j]){
                             cont++;
                         }
                 }
-
                 }
-                if(cont==3){
+                if(cont==6){
                     puntaje_del_juego[8]=40;
-                    if(cont==3&&lanzar_dado==2){
+                    if(cont==6&&lanzar_dado==2){
                         puntaje_del_juego[8]=puntaje_del_juego[8]+5;
                     }
                 }
@@ -276,7 +231,7 @@ int anotar(int lanzar_dado(int lanzada), int puntaje_del_juego[J]){
                 int cont=0;
                 int cont1=0;
                 for(i=0; i<P; i++){
-                for(j=i+1; j<P-1; j++){
+                for(j=i+1; j<P; j++){
                         if(lanzada[i]==lanzada[j]){
                             cont++;
                         }
@@ -284,25 +239,23 @@ int anotar(int lanzar_dado(int lanzada), int puntaje_del_juego[J]){
 
                 }
                 for(i=cont; i<P; i++){
-                for(j=i+1; j<P-1; j++){
+                for(j=i+1; j<P; j++){
                         if(lanzada[i]==lanzada[j]){
                             cont1++;
                         }
                 }
                 }
-                if(cont1+cont==3){
+                if(cont1+cont==4){
                     puntaje_del_juego[9]=30;
-                    if(cont+cont1==3&&lanzar_dado==2){
+                    if(cont+cont1==4&&lanzar_dado==2){
                         puntaje_del_juego[9]=puntaje_del_juego[9]+5;
                     }
                 }
             }
             break;
         }
-
         }while(puntaje_del_juego[posicion_juego_anotado]==-1);
-
-
+return 0;
 }
 int imprimir_puntajes(int puntaje_del_juego[J]){
         for(int i=0; i<J; i++){
@@ -311,8 +264,8 @@ int imprimir_puntajes(int puntaje_del_juego[J]){
         printf("\n");
         for(int j=0; j<J; j++){
             printf("%d      ", puntaje_del_juego[j]);
-
         }
+        return 0;
 }
 
 int main()
@@ -332,12 +285,9 @@ int main()
             }
         }
         }
-
             printf("PUNTAJES DEL JUGADOR 1\n");
             imprimir_puntajes(jugador1);
             printf("\n");
             printf("PUNTAJES DEL JUGADOR 2\n");
             imprimir_puntajes(jugador2);
-
-
 }
